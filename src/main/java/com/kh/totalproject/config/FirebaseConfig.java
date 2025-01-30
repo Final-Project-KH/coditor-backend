1caeecfd7152bc0dba30034cb4c78716d2ab3309
Tea-Wha <htw7880@gmail.com>
2025-01-30 14:28:08 +0900

    firebase 문제 해결

--- src/main/java/com/kh/totalproject/config/FirebaseConfig.java
+++ src/main/java/com/kh/totalproject/config/FirebaseConfig.java
@@ -4,6 +4,7 @@ import com.google.api.client.http.apache.v2.ApacheHttpTransport;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import io.github.cdimascio.dotenv.Dotenv;
 import jakarta.annotation.PostConstruct;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.http.client.HttpClient;
@@ -11,16 +12,27 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Base64;
 
 @Configuration
 public class FirebaseConfig {
 
     @PostConstruct
     public void initFirebase() throws IOException {
-        InputStream serviceAccount =
-                new ClassPathResource("firebase-service-account.json").getInputStream();
+
+        Dotenv dotenv = Dotenv.load();
+
+        String firebaseBase64 = dotenv.get("FIREBASE_CREDENTIALS");
+
+        if (firebaseBase64 == null || firebaseBase64.isEmpty()){
+            throw new RuntimeException("FIREBASE_CREDENTIALS 환경변수가 설정되지 않았습니다.");
+        }
+
+        byte[] decodedBytes = Base64.getDecoder().decode(firebaseBase64);
+        ByteArrayInputStream serviceAccount = new ByteArrayInputStream(decodedBytes);
 
         FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
